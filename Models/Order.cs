0710f966058fee87278837ff181247c9d6180f4e
fmings <mingsfelicia@gmail.com>
2024-08-20 21:04:09 -0500

    models created

---
+++ Models/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace bangazon.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerId { get; set; }
+        public bool Open { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int PaymentId { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
