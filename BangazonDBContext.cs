2e5e635d7a0ebdbc80d16646eb393900060312c0
fmings <mingsfelicia@gmail.com>
2024-08-21 19:46:25 -0500

    added db context components

--- BangazonDBContext.cs
+++ BangazonDBContext.cs
@@ -1,6 +1,29 @@
-﻿namespace bangazon
+﻿using Microsoft.EntityFrameworkCore;
+using bangazon.Data;
+using bangazon.Models;
+
+
+namespace bangazon
 {
-    public class BangazonDBContext
+    public class BangazonDBContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<ProductItem> ProductItems { get; set; }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public BangazonDBContext(DbContextOptions<BangazonDBContext> options) : base(options) 
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(UserData.Users);
+            modelBuilder.Entity<ProductItem>().HasData(ProductItemData.ProductItems);
+        }
     }
+
+    
 }
--- Data/UserData.cs
+++ Data/UserData.cs
@@ -4,7 +4,7 @@ namespace bangazon.Data
 {
     public class UserData
     {
-        public static List<User> ProductItems = new List<User>
+        public static List<User> Users = new List<User>
         {
             new User { Id = "1", FirstName = "Felicia", LastName = "Yahoo", Email = "felicia_mings@yahoo.com", Seller = false },
             new User { Id = "2", FirstName = "Lola", LastName = "Gmail", Email = "felicia.mings13@gmail.com", Seller = false }
