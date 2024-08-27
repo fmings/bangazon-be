f2c41bb609539d262134208ba4970a0c9d5af569
fmings <mingsfelicia@gmail.com>
2024-08-26 19:19:36 -0500

    updated order item API

--- API/OrderItemAPI.cs
+++ API/OrderItemAPI.cs
@@ -1,4 +1,5 @@
-﻿using bangazon.Models;
+﻿using System.Security.Claims;
+using bangazon.Models;
 
 namespace bangazon.API
 {
@@ -28,11 +29,46 @@ namespace bangazon.API
             });
 
             // POST ORDER ITEM (FOR ADDING ITEM TO CART)
-            app.MapPost("/api/orderitem", (BangazonDBContext db, OrderItem orderitem) =>
+            app.MapPost("/api/orderitem", (BangazonDBContext db, OrderItem orderItem, HttpContext context) =>
             {
-                db.OrderItems.Add(orderitem);
+                string guestId = null;
+                var user = context.User.Identity.IsAuthenticated ? context.User : null;
+
+                Order openOrder;
+
+                if (user != null)
+                {
+                    var userId = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                    openOrder = db.Orders.SingleOrDefault(oo => oo.Open && oo.CustomerId == userId);
+
+                }
+                else
+                {
+                    if (!context.Request.Cookies.TryGetValue("GuestId", out guestId))
+                    {
+                        // Generate a new GuestId if it doesn't exist
+                        guestId = Guid.NewGuid().ToString();
+                        context.Response.Cookies.Append("GuestId", guestId);
+                    }
+                    openOrder = db.Orders.SingleOrDefault(oo => oo.Open && oo.GuestId == guestId);
+                }
+
+                if (openOrder == null)
+                {
+                    openOrder = new Order
+                    {
+                        CustomerId = user?.FindFirst(ClaimTypes.NameIdentifier)?.Value,
+                        Open = true,
+                        GuestId = guestId
+                    };
+                    db.Orders.Add(openOrder);
+                    db.SaveChanges();
+                }
+                orderItem.OrderId = openOrder.Id;
+
+                db.OrderItems.Add(orderItem);
                 db.SaveChanges();
-                return Results.Created($"/api/orderitem/{orderitem.Id}", orderitem);
+                return Results.Created($"/api/orderitem/{orderItem.Id}", orderItem);
             });
 
         }
---
+++ Migrations/20240827000802_GuestId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bangazon.Migrations
+{
+    /// <inheritdoc />
+    public partial class GuestId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GuestId",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GuestId",
+                table: "Orders");
+        }
+    }
+}
